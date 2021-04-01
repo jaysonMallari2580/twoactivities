f5dd146a962621b7aed4a8848b430839e37c2720
jmallarijava@gmail.com <jmallarijava@gmail.com>
2021-04-01 08:47:15 -0400

    two activities send message

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.EditText;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -16,21 +20,42 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE =
             "com.example.android.twoactivities.extra.MESSAGE";
 
-    private EditText mMessageEditText;
+    private TextInputLayout mMessageEditText;
+    public static final int TEXT_REQUEST = 1;
+
+    private MaterialTextView mReplyHeadTextView;
+    private MaterialTextView mReplyTextView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMessageEditText = findViewById(R.id.editText_main);
+        mMessageEditText = (TextInputLayout)findViewById(R.id.editText_main);
+        mReplyHeadTextView = (MaterialTextView)findViewById(R.id.text_header_reply);
+        mReplyTextView = (MaterialTextView)findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view){
         Log.d(LOG_TAG, "Button clicked");
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
+        String message = mMessageEditText.getEditText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
